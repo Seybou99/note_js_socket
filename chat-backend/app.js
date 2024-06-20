022b6903204e852d4065d4b6856af32e8ab12f9a
Seybou99 <doumbiaseyboudiokolo@gmail.com>
2024-06-20 15:20:52 +0200

    mise à jour

--- chat-backend/app.js
+++ chat-backend/app.js
@@ -1,42 +1,64 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 const port = 3000;
-const cors = require("cors");
-const socket = require("socket.io");
-const http = require("http");
+const cors = require('cors');
+const socket = require('socket.io');
+const http = require('http');
 
 app.use(cors());
+// Je démarre un serveur HTTP, dans lequel je lance un serveur socket.io
+// On utilise cors pour ne pas avoir d'erreurs Cross Origin Request (ici on est censé accepter uniquement l'url du front)
 const server = http.createServer(app);
 const io = socket(server, {
-    cors: {origin: 'http://localhost:5173'}
-    });
-
-let socketConnected = new Set();
+  cors: {origin: 'http://localhost:5173'}
+})
+// Une fois le serveur sockets.io démarré, on va pouvoir créer différents évènements qui seront déclenchés par le front.
+// Le premier évènement est l'évènement de connexion, c'est ici ou l'on feras la grosse partie du code
+let socketsConnected = new Set();
 let users = {};
 
+// On crée un évènement sur le serveur, lorsque un user est connecté, on console log, on rajoute son id dans un tableau.
+// Et on peut créer différents évènements
 io.on('connection', (socket) => {
-    console.log(`New user connected: ${socket.id} `);
-    socketConnected.add(socket.id);
+  console.log(`New user connected : ${socket.id}`);
+  socketsConnected.add(socket.id);
 
-    io.emit('userCount', socketConnected.size);
-    console.log(socketConnected.size);
+  // Je transmet au front le nombre de sockets connectés au serveur
+  io.emit('userCount', socketsConnected.size);
 
-    socket.on('setUserName', (userName) => {
-        users[socket.id] = userName;
-        io.emit('updateUserList', users);
-    });
-    socket.on('disconnect', () => {
-        console.log(`User disconnected: ${socket.id}`);
-        socketConnected.delete(socket.id);
-        delete users[socket.id];
-        io.emit('updateUserList', users);
-        io.emit('userCount', socketConnected.size);
-        console.log(socketConnected.size);
-    })
-});
-app.get("/", (req, res) => {
-    res.send("Hello welcome to my server!");
+  socket.on('message', (message) => {
+    io.emit('message', message);
+  })
+
+  socket.on('typing', (user) => {
+    socket.broadcast.emit('typing', user);
+  })
+
+  socket.on('stopTyping', () => {
+    socket.broadcast.emit('stopTyping');
+  })
+
+  // Je retransmet a tout le monde, lorsqu'un user individuel setUsername
+  socket.on('setUsername', (username) => {
+    users[socket.id] = username;
+    io.emit('updateUserList', users);
+  });
+
+  // Nous allons gérer la déconnexion des users :
+  socket.on('disconnect', () => {
+    console.log(`User disconnected : ${socket.id}`);
+    socketsConnected.delete(socket.id);
+    delete users[socket.id];
+    io.emit('updateUserList', users);
+    io.emit('userCount', socketsConnected.size);
+  })
+
 })
+
+app.get('/', (req,res) => {
+  res.send('Hello, welcome to my server');
+})
+
 server.listen(port, () => {
-    console.log(`Example app listening on port http://localhost:${port}`)
+  console.log(`Server online on port http://localhost:${port}`);
 })
--- chat-frontend/src/App.jsx
+++ chat-frontend/src/App.jsx
@@ -1,24 +1,72 @@
-import './App.css'
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPaperPlane, faUser } from '@fortawesome/free-solid-svg-icons';
-import { useState } from 'react';
-import {io} from 'socket.io-client';
+import { useEffect, useState } from 'react';
+import { io } from 'socket.io-client';
 
-const socket = io('http://localhost:3000/');
+const socket = io('http://localhost:3000');
 
 function App() {
-  // On déclare 2 states, donc, l'equivalent des variables, qui me permettront de conserver, 
-  // le name ainsi que le message
+  // Déclaration des états
   const [name, setName] = useState('anonymous');
   const [message, setMessage] = useState('');
+  const [feedback, setFeedback] = useState('');
+  const [messages, setMessages] = useState([]);
+  const [userCount, setUserCount] = useState(0);
+  const [users, setUsers] = useState([]); // Utilisation d'un tableau pour les utilisateurs
+
+  useEffect(() => {
+    socket.on('message', (message) => {
+      setMessages((prevMessages) => [...prevMessages, message]);
+    });
+
+    socket.on('userCount', (userTotal) => {
+      setUserCount(userTotal);
+    });
+
+    socket.on('users', (usersList) => {
+      setUsers(usersList); // Mise à jour de l'état des utilisateurs
+    });
+
+    socket.on('typing', (user) => {
+      setFeedback(`${user.name} is typing...`);
+    });
+
+    socket.on('stopTyping', () => {
+      setFeedback('');
+    });
+
+    return () => {
+      socket.off('message');
+      socket.off('userCount');
+      socket.off('users');
+      socket.off('typing');
+      socket.off('stopTyping');
+    };
+  }, []);
 
   const handleNameChange = (e) => {
-    setName(e.target.value); // Ici on prend la valeur de l'input qui vient d'etre modifiée, et on utilise SetName pour changer la valeur du name
-  }
+    setName(e.target.value);
+    socket.emit('setUsername', e.target.value);
+  };
 
   const handleMessageChange = (e) => {
-    setMessage(e.target.value); // Ici on prend la valeur de l'input qui vient d'etre modifiée, et on utilise SetName pour changer la valeur du name
-  }
+    setMessage(e.target.value);
+    socket.emit('typing', { name });
+  };
+
+  const handleMessageSend = (e) => {
+    e.preventDefault();
+    const newMessage = {
+      text: message,
+      author: name,
+      date: new Date().toLocaleString(),
+      senderId: socket.id,
+    };
+    socket.emit('message', newMessage);
+    setMessage('');
+    socket.emit('stopTyping');
+  };
 
   return (
     <>
@@ -26,56 +74,61 @@ function App() {
       <div className="mainChat">
         <div className="flex">
           <div className="userList">
-            <h3>Users : 0</h3>
+            <h3>Users : {userCount}</h3>
             <ul>
-              <li>All</li>
-              <li>Toto</li>
-              <li>Bob</li>
-              <li>Alice</li>
+              {users.map((user, index) => (
+                <li key={index}>{user}</li>
+              ))}
             </ul>
           </div>
           <div className="chat">
             <div className="name">
-            <span className="nameForm">
-              <FontAwesomeIcon icon={faUser} />
-              <input type="text"
-                className="nameInput"
-                id="nameInput"
-                value={name}
-                onChange={handleNameChange}
-                maxLength="20"
+              <span className="nameForm">
+                <FontAwesomeIcon icon={faUser} />
+                <input 
+                  type="text"
+                  className="nameInput"
+                  id="nameInput"
+                  value={name}
+                  onChange={handleNameChange}
+                  maxLength="20"
+                />
+              </span>
+            </div>
+            <ul className="conversation">
+              {messages.map((msg, index) => (
+                <li key={index} className={msg.senderId === socket.id ? 'messageRight' : 'messageLeft'}>
+                  <p className="message">{msg.text}</p>
+                  <span>{msg.author} - {msg.date}</span>
+                </li>
+              ))}
+              {feedback && (
+                <li className="messageFeedback">
+                  <p className="feedback">{feedback}</p>
+                </li>
+              )}
+            </ul>
+            <form className="messageForm" onSubmit={handleMessageSend}>
+              <input 
+                type="text"
+                name="message"
+                className='messageInput'
+                value={message}
+                onKeyUp={() => {
+                  if (!message) {
+                    socket.emit('stopTyping');
+                  }
+                }}
+                onChange={handleMessageChange}
               />
-            </span>
-          </div>
-          <ul className="conversation">
-            <li className="messageLeft">
-              <p className="message">Bonjour tout le monde !</p>
-              <span>author - 18 juin 2024</span>
-            </li>
-            <li className="messageRight">
-              <p className="message">Ca va?</p>
-              <span>author - 18 juin 2024</span>
-            </li>
-            <li className="messageFeedback">
-              <p className="feedback">Toto is typing...</p>
-            </li>
-          </ul>
-          <form className="messageForm">
-            <input type="text" 
-              name="message" 
-              className='messageInput' 
-              value={message}
-              onChange={handleMessageChange} 
-            />
-            <div className="vDivider"></div>
-            <button type="submit" className='sendButton'>Send <FontAwesomeIcon icon={faPaperPlane}/></button>
-          </form>
+              <div className="vDivider"></div>
+              <button type="submit" className='sendButton'>Send <FontAwesomeIcon icon={faPaperPlane} /></button>
+            </form>
           </div>
         </div>
-
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
